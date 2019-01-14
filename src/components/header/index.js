ca05d4493a68d50c63b0c07801c0ceda16adfa1c
bujakasha <casperv96@gmail.com>
2019-01-15 00:38:05 +0200

    small

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,7 +9,6 @@ const Header = ({carouselImages, children}) => (
       <div className="order-1 order-md-0 col-md-4 col-md-6 pt-4 pt-sm-0">{children}</div>
       <div className="pull-md-12 col-md-6 col-lg-5 offset-lg-1 px-0 px-sm-3">
       <SwSwiper swiperData={carouselImages} />
-       
       </div>
     </div>
   </div>
